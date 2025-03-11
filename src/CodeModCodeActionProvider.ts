0c89611c56b260aead9d0cb6bda9f6a2ca5eda70
nav4e <yodelinping@126.com>
2025-03-11 16:35:42 +0800

    feat: 判断在  tag 内

--- src/CodeModCodeActionProvider.ts
+++ src/CodeModCodeActionProvider.ts
@@ -1,32 +1,44 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-import { commandIds, isSupportedLanguage } from './const';
-import { isSelection } from './utils/isSelection';
+import { commandIds, isSupportedLanguage } from "./const";
+import { isSelection } from "./utils/isSelection";
 
 export class CodeModCodeActionProvider implements vscode.CodeActionProvider {
-    public async provideCodeActions(
-        document: vscode.TextDocument,
-        selection: vscode.Range | vscode.Selection,
-        context: vscode.CodeActionContext,
-        token: vscode.CancellationToken
-    ): Promise<vscode.Command[]> {
-        if (!isSupportedLanguage(document.languageId)) {
-            vscode.window.showInformationMessage('not supported language');
-            return [];
-        }
-        vscode.window.showInformationMessage('supported language');
-        // if (!isSelection(selection)) {
-        //     // Complex commands, run-on-save etc
-        //     return [];
-        // }
-
-        return [
-            {
-                command: 'editor.emmet.action.wrapWithAbbreviation',
-                title: 'Wrap with Abbreviation',
-                arguments: [],
-                tooltip: 'Wrap with Abbreviation',
-            }
-        ];
+  public async provideCodeActions(
+    document: vscode.TextDocument,
+    selection: vscode.Range | vscode.Selection,
+    context: vscode.CodeActionContext,
+    token: vscode.CancellationToken
+  ): Promise<vscode.Command[]> {
+    if (!isSupportedLanguage(document.languageId)) {
+      return [];
     }
+
+    // @ts-ignore
+    const line = document.lineAt(selection.active.line).text;
+    if (!line.includes("<")) {
+      return [];
+    }
+
+    return [
+      {
+        command: "editor.emmet.action.wrapWithAbbreviation",
+        title: "Emmet: Wrap with Abbreviation",
+        arguments: [],
+        tooltip: "Emmet: Wrap with Abbreviation",
+      },
+      {
+        command: "editor.emmet.action.removeTag",
+        title: "Emmet: Remove Tag",
+        arguments: [],
+        tooltip: "Emmet: Remove Tag",
+      },
+      {
+        command: "editor.emmet.action.splitJoinTag",
+        title: "Emmet: Split Join Tag",
+        arguments: [],
+        tooltip: "Emmet: Split Join Tag",
+      },
+    ];
+  }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,6 @@ import { supportedLanguages } from "./const";
 import { CodeModCodeActionProvider } from "./CodeModCodeActionProvider";
 
 export function activate(context: ExtensionContext) {
-  vscode.window.showInformationMessage("extension activated");
   context.subscriptions.push(
     languages.registerCodeActionsProvider(
       supportedLanguages,
